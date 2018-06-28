d0e91b90949879ea2086abc1a65b943b29ca121b
wangliu <wangliu@isoftstone.com>
2018-06-28 09:28:06 +0800

    调整设置选项顺序

--- src/services/dbservices/ORMService.ts
+++ src/services/dbservices/ORMService.ts
@@ -276,11 +276,11 @@ export class LotteryDbService {
                         if (!res) {
                             return Setting.bulkCreate([
                                 {key: 'isRealInvest', value: '0', orderId: '1', desc: '是否是真实投注 1:真实 0:模拟'},
-                                {key: 'awardMode', value: '100', orderId: '2', desc: '元、角、分、厘模式'},
-                                {key: 'touZhuBeiShu', value: '2', orderId: '3', desc: '投注倍数'},
-                                {key: 'originAccountBalance', value: '1000', orderId: '4', desc: '账户初始余额'},
-                                {key: 'maxAccountBalance', value: '1200', orderId: '5', desc: '最大盈利目标金额'},
-                                {key: 'minAccountBalance', value: '0', orderId: '6', desc: '最大亏损金额'},
+                                {key: 'touZhuBeiShu', value: '2', orderId: '2', desc: '投注倍数'},
+                                {key: 'originAccountBalance', value: '1000', orderId: '3', desc: '账户初始余额'},
+                                {key: 'maxAccountBalance', value: '1200', orderId: '4', desc: '最大盈利目标金额'},
+                                {key: 'minAccountBalance', value: '0', orderId: '5', desc: '最大亏损金额'},
+                                {key: 'awardMode', value: '100', orderId: '6', desc: '元、角、分、厘模式'},
                                 {key: 'currentSelectedInvestPlanType', value: '1', orderId: '7', desc: '当前选择的投注方案类型'}
                             ]);
                         } else {
