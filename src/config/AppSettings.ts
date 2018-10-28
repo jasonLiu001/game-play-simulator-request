1bc26b2b592d6b94be9ccff2b96d6f541c93acff
jasonLiu001 <wangliu@isoftstone.com>
2018-10-28 22:10:50 +0800

    应用程序设置统一到文件

--- src/config/AppSettings.ts
+++ src/config/AppSettings.ts
@@ -10,9 +10,9 @@ export class AppSettings {
     //是否用相反的投注号码进行投注
     public static isUseReverseInvestNumbers = false;
     //最低利润预警值
-    public static minProfitNotification: number = 2800;
+    public static minProfitNotification: number = 4500;
     //最高利润预警
-    public static maxProfitNotification: number = 6000;
+    public static maxProfitNotification: number = 7000;
     //自动投注截止时间
     public static investEndTime: string = "21:59:00";
 }
