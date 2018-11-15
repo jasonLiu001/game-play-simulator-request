b925dccdee9bd79ec070659ff43546116943283f
jasonLiu001 <wangliu@isoftstone.com>
2018-11-15 13:37:27 +0800

    添加手工执行方法

--- src/controller/AwardController.ts
+++ src/controller/AwardController.ts
@@ -1,10 +1,32 @@
 import {Request, Response} from 'express';
+import {ScheduleTaskList} from "../config/ScheduleTaskList";
+import {AppServices} from "../services/AppServices";
+import {NotificationService} from "../services/notification/NotificationService";
+
+let notificationService = new NotificationService();
 
 export class AwardController {
     public getAward(req: Request, res: Response) {
         res.status(200).send({
             message: 'GET request successfulll!!!!'
         })
+    }
+
+    public startApp(req: Request, res: Response) {
+        if (ScheduleTaskList.appStartTaskEntity.cronSchedule == null) {
+            //启动投注程序
+            AppServices.start();
+            //启动通知程序
+            notificationService.start();
+            res.status(200).send({
+                message: 'Success! App started.'
+            })
+        } else {
+            res.status(200).send({
+                message: 'Failed! App had already started.'
+            })
+        }
+
     }
 }
 
