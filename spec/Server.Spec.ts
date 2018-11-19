397319c6754adbb168e3a8a7644052eeed205188
jasonLiu001 <wangliu@isoftstone.com>
2018-11-19 16:31:50 +0800

    接口名称修改

--- spec/Server.Spec.ts
+++ spec/Server.Spec.ts
@@ -1,31 +1,4 @@
 import "jasmine";
-// import cron = require('node-cron');
-// import app from "../src/App";
-//
-// let task2: any = null;
-// let task3: any = null;
-// let task1: any = cron.schedule('26 23 * * *', () => {
-//     console.log('started..');
-//     task2 = cron.schedule('*/2 * * * * *', () => {
-//         console.log('I am task2');
-//     });
-//     task3 = cron.schedule('*/4 * * * * *', () => {
-//         console.log('I am task3');
-//     });
-// });
-//
-// let task4: any = cron.schedule('27 23 * * *', () => {
-//     task2.destroy();
-//     task3.destroy();
-//     console.log('I will destory task2 and task3');
-//     console.log('task2=%s', task2);
-//     console.log('task3=%s', task3);
-// });
-//
-// const PORT = 3000;
-// app.listen(PORT, () => {
-//     console.log('Express server listening on port ' + PORT);
-// });
 
 describe('Our awesome test', () => {
     it('tests something completely non-sense', () => {
