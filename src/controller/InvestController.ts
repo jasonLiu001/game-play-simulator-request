6853cc92fa71d39489d29e306cc38af825ecec3c
jasonLiu001 <wangliu@isoftstone.com>
2018-12-02 00:13:51 +0800

    接口调用添加日志

--- src/controller/InvestController.ts
+++ src/controller/InvestController.ts
@@ -7,6 +7,7 @@ import {PlatformService} from "../services/platform/PlatformService";
 import {DefaultRequest} from "../services/AppServices";
 import {ResponseJson} from "../models/ResponseJson";
 import {TimeService} from "../services/time/TimeService";
+import {CONFIG_CONST} from "../config/Config";
 
 let log4js = require('log4js'),
     log = log4js.getLogger('InvestController');
@@ -28,6 +29,10 @@ export class InvestController {
         let touZhuBeiShu: number = Number(req.body.touZhuBeiShu);//投注倍数
         let investTableName: string = req.body.investTableName;//投注的表名
         let investNumbers: string = req.body.investNumbers;//投注号码
+
+        //记录当前设置中的元角分模式 投注成功或者失败后 恢复到当前状态
+        let configAwardMode: number = CONFIG_CONST.awardMode;
+
         let jsonRes: ResponseJson = new ResponseJson();
         log.info('手动一键投注请求已收到，参数:period=%s,planType=%s,awardMode=%s,touZhuBeiShu=%s,investTableName=%s', period, planType, awardMode, touZhuBeiShu, investTableName);
 
@@ -52,6 +57,8 @@ export class InvestController {
                     return Promise.reject(investInfo.period + "期已结束，投注失败");
                 }
                 //根据参数更改投注信息
+                CONFIG_CONST.awardMode = awardMode;//同时修改设置中的awardMode，异常或者成功需要恢复
+                log.info('手动投注模式值更改为 %s', CONFIG_CONST.awardMode);
                 investInfo.awardMode = awardMode;
                 investInfo.touZhuBeiShu = touZhuBeiShu;
                 investInfo.investNumbers = investNumbers;
@@ -64,12 +71,18 @@ export class InvestController {
                 log.info("%s，当前时间：%s", successMsg, moment().format('YYYY-MM-DD HH:mm:ss'));
                 //投注成功 更新已投注期号 防止重复投注
                 InvestControllerConfig.investPeriod = period;
+                //恢复设置中投注模式
+                CONFIG_CONST.awardMode = configAwardMode;
+                log.info('手动投注结束，投注模式值恢复为 %s', CONFIG_CONST.awardMode);
                 return res.status(200).send(jsonRes);
             })
             .catch((e) => {
                 let errMsg: string = period + "期，一键投注失败";
                 jsonRes.fail(errMsg, e.message);
                 log.info("%s，当前时间：%s", errMsg, moment().format('YYYY-MM-DD HH:mm:ss'));
+                //恢复设置中投注模式
+                CONFIG_CONST.awardMode = configAwardMode;
+                log.info('手动投注结束，投注模式值恢复为 %s', CONFIG_CONST.awardMode);
                 return res.status(200).send(jsonRes);
             });
     }
