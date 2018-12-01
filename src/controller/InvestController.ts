a0a3e1017b22a4f3a1d4252c608145f38baeb523
jasonLiu001 <wangliu@isoftstone.com>
2018-12-01 23:43:02 +0800

    接口调用添加日志

--- src/controller/InvestController.ts
+++ src/controller/InvestController.ts
@@ -30,22 +30,28 @@ export class InvestController {
         let investTableName: string = req.body.investTableName;//投注的表名
         let investNumbers: string = req.body.investNumbers;//投注号码
         let jsonRes: ResponseJson = new ResponseJson();
+        log.info('手动一键投注请求已收到，参数:period=%s,planType=%s,awardMode=%s,touZhuBeiShu=%s,investTableName=%s', period, planType, awardMode, touZhuBeiShu, investTableName);
 
         //当前能够投注的期号
         let currentPeriod: string = TimeService.getCurrentPeriodNumber(new Date());
         if (period != currentPeriod) {
             jsonRes.fail(period + "期已停止投注，投注失败");
+            log.error("手动投注失败，%s期已停止投注，当前时间：%s", period, moment().format('YYYY-MM-DD HH:mm:ss'));
             return res.status(200).send(jsonRes);
         }
 
         if (InvestControllerConfig.investPeriod == period) {
             jsonRes.fail(period + "期已投注，勿重复投注");
+            log.error("手动投注失败，%s期重复投注，当前时间：%s", period, moment().format('YYYY-MM-DD HH:mm:ss'));
             return res.status(200).send(jsonRes);
         }
 
         LotteryDbService.getInvestByTableName(investTableName, period, planType)
             .then((investInfo: InvestInfo) => {
-                if (investInfo.status == 1) return Promise.reject(investInfo.period + "期已结束，投注失败");
+                if (investInfo.status == 1) {
+                    log.error("手动投注失败，%s期已完成，不允许投注，当前时间：%s", period, moment().format('YYYY-MM-DD HH:mm:ss'));
+                    return Promise.reject(investInfo.period + "期已结束，投注失败");
+                }
                 //根据参数更改投注信息
                 investInfo.awardMode = awardMode;
                 investInfo.touZhuBeiShu = touZhuBeiShu;
@@ -55,6 +61,7 @@ export class InvestController {
             .then(() => {
                 let successMsg: string = period + "期，一键投注成功";
                 jsonRes.success(successMsg);
+                log.info("%s，当前时间：%s", successMsg, moment().format('YYYY-MM-DD HH:mm:ss'));
                 //投注成功 更新已投注期号 防止重复投注
                 InvestControllerConfig.investPeriod = period;
                 return res.status(200).send(jsonRes);
@@ -62,6 +69,7 @@ export class InvestController {
             .catch((e) => {
                 let errMsg: string = period + "期，一键投注失败";
                 jsonRes.fail(errMsg, e.message);
+                log.info("%s，当前时间：%s", errMsg, moment().format('YYYY-MM-DD HH:mm:ss'));
                 return res.status(200).send(jsonRes);
             });
     }
