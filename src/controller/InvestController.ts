ff8832a2f4a275476108176f0ef0171b943e220a
jasonLiu001 <wangliu@isoftstone.com>
2018-12-01 13:35:50 +0800

    重复投注限制

--- src/controller/InvestController.ts
+++ src/controller/InvestController.ts
@@ -12,17 +12,28 @@ let investBase = new InvestBase(),
     log4js = require('log4js'),
     log = log4js.getLogger('InvestController');
 
+class InvestControllerConfig {
+    //已经投注的期号
+    public static investPeriod: string = null;
+}
+
 export class InvestController {
     /**
      *
      * 根据计划执行一键投注
      */
-    public execute(req: Request, res: Response) {
+    public execute(req: Request, res: Response): any {
         let period: string = req.body.period;
         let planType: number = req.body.planType;
         let awardMode: number = req.body.awardMode;//投注模式
         let touZhuBeiShu: number = req.body.touZhuBeiShu;//投注倍数
         let investTableName: string = req.body.investTableName;//投注的表名
+
+        if (InvestControllerConfig.investPeriod == period) {
+            let jsonRes: ResponseJson = new ResponseJson();
+            jsonRes.fail(period + "期已投注，勿重复投注");
+            return res.status(200).send(jsonRes);
+        }
 
         LotteryDbService.getInvestByTableName(investTableName, period, planType)
             .then((investInfo: InvestInfo) => {
@@ -32,13 +43,15 @@ export class InvestController {
                 return PlatformService.loginAndInvest(DefaultRequest.request, investInfo);
             })
             .then(() => {
-                let successMsg: string = "一键投注成功";
+                let successMsg: string = period + "期，一键投注成功";
                 let jsonRes: ResponseJson = new ResponseJson();
                 jsonRes.success(successMsg);
+                //投注成功 更新已投注期号 防止重复投注
+                InvestControllerConfig.investPeriod = period;
                 return res.status(200).send(jsonRes);
             })
             .catch((e) => {
-                let errMsg: string = "一键投注失败";
+                let errMsg: string = period + "期，一键投注失败";
                 let jsonRes: ResponseJson = new ResponseJson();
                 jsonRes.fail(errMsg, e.message);
                 return res.status(200).send(jsonRes);
