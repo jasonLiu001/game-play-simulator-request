5e52ac5cb409b6ece359d3c48cafae174d3953b2
jasonLiu001 <wangliu@isoftstone.com>
2018-12-09 18:49:04 +0800

    bug修复

--- src/static/js/apiList.js
+++ src/static/js/apiList.js
@@ -2,6 +2,6 @@ var servrContextPath = "/service";
 var apiList = {
     findAllInvestTotalInfoList: servrContextPath + "/lottery/findAllInvestTotalInfoList",
     getInvestTotalInfoByPeriod: servrContextPath + "/lottery/getInvestTotalInfoByPeriod",
-    findInvestInfoList: servrContextPath + "/lottery/findInvestInfoList?pageIndex=1&pageSize={0}&planType={1}&createDateStr={2}",
-    findInvestTotalInfoList: servrContextPath + "/lottery/findInvestTotalInfoList?pageIndex=1&pageSize={0}&planType={1}&createDateStr={2}",
+    findInvestInfoList: servrContextPath + "/lottery/findInvestInfoList?pageIndex=1&pageSize={0}&planType={1}&createTimeStr={2}",
+    findInvestTotalInfoList: servrContextPath + "/lottery/findInvestTotalInfoList?pageIndex=1&pageSize={0}&planType={1}&createTimeStr={2}",
 };
