8ff2455b0465280362c19a29c14717336ca7a6d2
jasonLiu001 <wangliu@isoftstone.com>
2018-12-09 12:01:43 +0800

    添加查询日期

--- src/static/js/echarts/investTotalCharts.js
+++ src/static/js/echarts/investTotalCharts.js
@@ -6,6 +6,9 @@ var app = new Vue({
     mixins: [utilsMixin],
     methods: {},
     created() {
+        //日期控件初始化
+        $('#datepicker').datepicker();
+
         //初始化图表
         this.initInvestInfoCharts(apiList.findInvestTotalInfoList.replace('{0}', 1), 'plan_01', 'invest_total', 1, 'InvestTotal_01');
         this.initInvestInfoCharts(apiList.findInvestTotalInfoList.replace('{0}', 2), 'plan_02', 'invest_total', 2, 'InvestTotal_02');
