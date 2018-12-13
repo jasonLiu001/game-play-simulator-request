248d0460559b77fdaab4a79ae1d75ed47bc9fbd1
jasonLiu001 <wangliu@isoftstone.com>
2018-12-13 17:40:31 +0800

    参数名称修改

--- src/static/js/apiList.js
+++ src/static/js/apiList.js
@@ -2,6 +2,6 @@ var servrContextPath = "/service";
 var apiList = {
     findAllInvestTotalInfoList: servrContextPath + "/lottery/findAllInvestTotalInfoList",
     getInvestTotalInfoByPeriod: servrContextPath + "/lottery/getInvestTotalInfoByPeriod",
-    findInvestInfoList: servrContextPath + "/lottery/findInvestInfoList?pageIndex=1&pageSize={0}&planType={1}&createTimeStr={2}&endTimeStr={3}",
-    findInvestTotalInfoList: servrContextPath + "/lottery/findInvestTotalInfoList?pageIndex=1&pageSize={0}&planType={1}&createTimeStr={2}&endTimeStr={3}",
+    findInvestInfoList: servrContextPath + "/lottery/findInvestInfoList?pageIndex=1&pageSize={0}&planType={1}&startTimeStr={2}&endTimeStr={3}",
+    findInvestTotalInfoList: servrContextPath + "/lottery/findInvestTotalInfoList?pageIndex=1&pageSize={0}&planType={1}&startTimeStr={2}&endTimeStr={3}",
 };
