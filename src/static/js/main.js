89eb4912b9b7b02a3ac5c4c3e7ad257a2ba4a1af
jasonLiu001 <wangliu@isoftstone.com>
2019-02-24 23:12:09 +0800

    首页初始化

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -70,7 +70,7 @@
 	var offcanvasMenu = function() {
 
 		$('#page').prepend('<div id="gtco-offcanvas" />');
-		$('#page').prepend('<a href="#" class="js-gtco-nav-toggle gtco-nav-toggle gtco-nav-white"><i></i></a>');
+		$('#page').prepend('<a href="javascript:void(0);" class="js-gtco-nav-toggle gtco-nav-toggle gtco-nav-white"><i></i></a>');
 		var clone1 = $('.menu-1 > ul').clone();
 		$('#gtco-offcanvas').append(clone1);
 		var clone2 = $('.menu-2 > ul').clone();
