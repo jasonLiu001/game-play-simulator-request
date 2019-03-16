26265ec500856a64cec52ffe048ca3a97c47924d
jasonLiu001 <wangliu@isoftstone.com>
2019-03-16 20:55:11 +0800

    启动app使用promise

--- src/controller/AppController.ts
+++ src/controller/AppController.ts
@@ -4,6 +4,7 @@ import {AppServices} from "../services/AppServices";
 import {ScheduleTaskList} from "../config/ScheduleTaskList";
 import {ResponseJson} from "../models/ResponseJson";
 import moment  = require('moment');
+import BlueBirdPromise = require('bluebird');
 import {ConstVars} from "../global/ConstVars";
 
 let log4js = require('log4js'),
@@ -15,7 +16,7 @@ export class AppController {
      *
      * 启动app
      */
-    public start(req: Request, res: Response): any {
+    public async start(req: Request, res: Response): BlueBirdPromise<any> {
         if (ScheduleTaskList.awardFetchTaskEntity.cronSchedule == null && ScheduleTaskList.notificationTaskEntity.cronSchedule == null) {
             //启动投注程序
             AppServices.start();
@@ -25,13 +26,13 @@ export class AppController {
             let msg: string = '恭喜！程序已成功启动!赚钱咯！';
             jsonRes.success(msg, msg);
             log.info('%s 当前时间：%s', msg, moment().format(ConstVars.momentDateTimeFormatter));
-            res.status(200).send(jsonRes);
+            return res.status(200).send(jsonRes);
         } else {
             let jsonRes: ResponseJson = new ResponseJson();
             let errMsg: string = '程序正在运行，无法重复启动!';
             jsonRes.fail(errMsg, errMsg);
             log.info('%s 当前时间：%s', errMsg, moment().format(ConstVars.momentDateTimeFormatter));
-            res.status(200).send(jsonRes);
+            return res.status(200).send(jsonRes);
         }
 
     }
