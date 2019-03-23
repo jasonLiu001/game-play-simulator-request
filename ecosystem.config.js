bc4364371241a4e8b6255b906839fead1a627d5a
jasonLiu001 <wangliu@isoftstone.com>
2019-03-23 11:42:38 +0800

    pm2 ecosystem renamed.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [{
         name: "GamePlayer",
         script: "./Server.js",
-        cwd: "/dist",//当前工作目录
+        cwd: "dist/",//当前工作目录
         max_memory_restart: '1G',//超过最大内存是重启程序
         node_args: "--max_old_space_size=8192",
         env: {
