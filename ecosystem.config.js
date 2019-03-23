b33c907cca920de25e60c100a25608cd4ef7bb25
jasonLiu001 <wangliu@isoftstone.com>
2019-03-23 12:20:07 +0800

    updated.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
         name: "GamePlayer",
         script: "./Server.js",
         cwd: "dist/",//当前工作目录
-        max_memory_restart: '1G',//超过最大内存是重启程序
+        max_memory_restart: '8G',//超过8G内存重启程序
         node_args: "--max_old_space_size=8192",
         env: {
             NODE_ENV: "production",
