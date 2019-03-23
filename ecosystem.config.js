1938fed2d7dc0f92dfd064b40748fb18d02a3ed1
jasonLiu001 <wangliu@isoftstone.com>
2019-03-23 12:22:23 +0800

    updated.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,10 @@
+/**
+ *
+ * PM2 的配置文件 切换到项目根目录 执行: pm2 start 可以直接启动项目
+ */
 module.exports = {
     apps: [{
-        name: "GamePlayer",
+        name: "Server",
         script: "./Server.js",
         cwd: "dist/",//当前工作目录
         max_memory_restart: '8G',//超过8G内存重启程序
