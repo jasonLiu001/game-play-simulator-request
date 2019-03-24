9e290f5933d55395abcbf23a3acf5bddff489dde
jasonLiu001 <wangliu@isoftstone.com>
2019-03-24 16:09:17 +0800

    add a new app.ts

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -16,19 +16,6 @@ module.exports = {
             env_development: {
                 NODE_ENV: "development",
             }
-        },
-        {
-            name: "App",
-            script: "./App.js",
-            cwd: "dist/",//当前工作目录
-            max_memory_restart: '8G',//超过8G内存重启程序
-            node_args: "--max_old_space_size=8192",
-            env: {
-                NODE_ENV: "production",
-            },
-            env_development: {
-                NODE_ENV: "development",
-            }
         }
     ]
 };
