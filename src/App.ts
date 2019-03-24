93e5be7853a07209bef0d66083d0126ee304e16e
jasonLiu001 <wangliu@isoftstone.com>
2019-03-24 15:06:32 +0800

    add a new app.ts

--- src/App.ts
+++ src/App.ts
@@ -1,8 +1,46 @@
-import {NotificationService} from "./services/notification/NotificationService";
 import {AppServices} from "./services/AppServices";
+import {ScheduleTaskList} from "./config/ScheduleTaskList";
+import {ConstVars} from "./global/ConstVars";
+import {NotificationService} from "./services/notification/NotificationService";
+import cron = require('node-cron');
+import moment  = require('moment');
 
-let notificationService = new NotificationService();
-//启动投注程序
-AppServices.start();
-//启动通知程序
-notificationService.start();
+let log4js = require('log4js'),
+    path = require('path');
+log4js.configure(path.resolve(__dirname, '.', 'config/log4js.json'));
+
+//日志文件
+let log = log4js.getLogger('App'),
+    notificationService = new NotificationService();
+
+//node-cron的格式可能和linux中crontab有区别，设置时请参考文档：https://www.npmjs.com/package/node-cron
+ScheduleTaskList.appStartTaskEntity.cronSchedule = cron.schedule(ScheduleTaskList.appStartTaskEntity.cronTimeStr, () => {
+    AppServices.initAppStartConfig();//重置应用运行参数
+    log.info('执行启动app任务，当前时间：%s', moment().format(ConstVars.momentDateTimeFormatter));
+    if (ScheduleTaskList.awardFetchTaskEntity.cronSchedule == null) {
+        //启动投注程序
+        AppServices.start();
+    } else {
+        log.error("主程序已启动，无需重复启动！")
+    }
+    if (ScheduleTaskList.notificationTaskEntity.cronSchedule == null) {
+        //启动通知程序
+        notificationService.start();
+    } else {
+        log.error("通知程序已启动，无需重复启动！")
+    }
+});
+
+//每天固定时间停止 预警及获取奖号计划任务
+ScheduleTaskList.appStopTaskEntity.cronSchedule = cron.schedule(ScheduleTaskList.appStopTaskEntity.cronTimeStr, () => {
+    log.info('主程序及子任务已终止，当前时间：%s', moment().format(ConstVars.momentDateTimeFormatter));
+    //销毁获取奖号
+    ScheduleTaskList.awardFetchTaskEntity.cronSchedule.destroy();
+    ScheduleTaskList.awardFetchTaskEntity.cronSchedule = null;
+    log.info('主程序已停止');
+    //销毁预警通知
+    ScheduleTaskList.notificationTaskEntity.cronSchedule.destroy();
+    ScheduleTaskList.notificationTaskEntity.cronSchedule = null;
+    log.info('通知程序已停止');
+    AppServices.initAppStartConfig();//重置应用运行参数
+});
