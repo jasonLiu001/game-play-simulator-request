4dac64d282cec2a36462da1d791c91dde6067433
jasonLiu001 <wangliu@isoftstone.com>
2019-03-24 15:55:49 +0800

    add a new app.ts

--- src/App.ts
+++ src/App.ts
@@ -13,19 +13,24 @@ log4js.configure(path.resolve(__dirname, '.', 'config/log4js.json'));
 let log = log4js.getLogger('App'),
     notificationService = new NotificationService();
 
+//启动投注程序
+AppServices.start();
+//启动通知程序
+notificationService.start();
+
 //node-cron的格式可能和linux中crontab有区别，设置时请参考文档：https://www.npmjs.com/package/node-cron
 ScheduleTaskList.appStartTaskEntity.cronSchedule = cron.schedule(ScheduleTaskList.appStartTaskEntity.cronTimeStr, () => {
     AppServices.initAppStartConfig();//重置应用运行参数
     log.info('执行启动app任务，当前时间：%s', moment().format(ConstVars.momentDateTimeFormatter));
     if (ScheduleTaskList.awardFetchTaskEntity.cronSchedule == null) {
         //启动投注程序
-        AppServices.start();
+        //AppServices.start();
     } else {
         log.error("主程序已启动，无需重复启动！");
     }
     if (ScheduleTaskList.notificationTaskEntity.cronSchedule == null) {
         //启动通知程序
-        notificationService.start();
+        //notificationService.start();
     } else {
         log.error("通知程序已启动，无需重复启动！");
     }
